52818510b5285b89504b2187585c76ec0116125a
JACK_SUSANK <jsusank5@gmail.com>
2024-02-15 18:12:12 -0800

    Added comments

--- booperapp1/models.py
+++ booperapp1/models.py
@@ -1,189 +1,42 @@
-import os
-from django.conf import settings
-
-
- 
 from django.db import models
 
-# Testing
-# Create your models here.
-# models.py
-
-print("models.py")
-
-
-# # Manually configure Django settings
-# if __name__ == "__main__":
-#     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "booper1.settings")
-#     settings.configure()
-
-
+'''
+This is the model for the BoopBoard. It contains a name, the number of columns, and the number of rows it has.
+It also contains a list of Boops, which are the individual buttons on the BoopBoard.
+I use SQLite3 as the database for this project, so the BoopBoard and Boop models are stored in the database.
+'''
 class BoopBoard(models.Model):
     name = models.CharField(max_length=100, default="Nameless BoopBoard")
     columns = models.IntegerField(default=4)
     rows = models.IntegerField(default=4)
 
-
-    # def __init__(self, columns, rows):
-    #     self.columns = columns
-    #     self.rows = rows
-    #     self.myBoops = []
-    #     i = 1
-    #     for thatColumn in range(self.columns):
-    #         for thatRow in range(self.rows):
-    #             i += 1
-    #             self.myBoops.append(Boop(column=thatColumn, row=thatRow, thisID=i))
-
-    def getMyBoops(self):
-        return self.myBoops
-
     def __str__(self):
-        return f"BoopBoard with {self.columns} columns and {self.rows} rows"
-    
+        return f"BoopBoard named {self.name} with {self.columns} columns and {self.rows} rows."
 
+
+'''
+This is the model for the Boop. It contains a reference to the BoopBoard it is on,
+the column and row it is in, and a boolean for whether it is booped or not.
+'''
 class Boop(models.Model):
-    boopboard = models.ForeignKey(BoopBoard, on_delete=models.CASCADE)
+    boopboard = models.ForeignKey(BoopBoard, on_delete=models.CASCADE) # This connects the Boops to the BoopBoard
     thisID = models.IntegerField(default=0)
     column = models.IntegerField()
     row = models.IntegerField()
     booped = models.BooleanField(default=False)
 
-    # def __init__(self, column, row, thisID):
-    #     self.column = column
-    #     self.row = row
-    #     self.booped = False
-    #     self.thisID = thisID
 
+    # This function toggles the booped boolean
     def toggle_booped(self):
         self.booped = not self.booped
         self.save()
 
-
+    # This function returns the ID (1-16) of the booped boolean
     def getThisID(self):
         return self.thisID
 
+    
     def __str__(self):
         return f"The Boop at {self.column} {self.row} is {'Booped' if self.booped else 'Not Booped'}"
 
 
-# Initializing the Boops and boopboard:
-    
-# thisBoopBoard = BoopBoard(collumns=4, rows=4)
-
-
-
-
-
-
-
-
-
-"""
-class ButtonState(models.Model):
-    name = models.CharField(max_length=100)
-    state = models.BooleanField(default=False)
-
-
-    def __str__(self):
-        return self.name
-"""
-
-
-"""
-class Boop:
-    def __init__(self, collumn, row):
-        self.collumn = collumn
-        self.row = row
-        self.booped = False
-    
-    def __str__(self):
-        return f"The Boop at {self.collumn} {self.row} is {self.booped}"
-    
-    def __isBooped__(self):
-        return self.booped
-    
-    def __boop__(self):
-        if self.booped == True:
-            self.booped = False
-        else:    
-            self.booped = True
-        print("I have been booped!")
-
-class BoopBoard:
-    def __init__(self, collumns, rows):
-        self.collumns = collumns
-        self.rows = rows
-        self.boops = []
-        for collumn in range(self.collumns):
-            self.boops.append([])
-            for row in range(self.rows):
-                self.boops[collumn].append(Boop(collumn, row))
-    
-    def __boop__(self, collumn, row):
-        self.boops[collumn][row].__boop__()
-
-    def __isBooped__(self, collumn, row):
-        return self.boops[collumn][row].__isBooped__()
-
-    def __str__(self):
-        string = ""
-        for row in range(self.rows):
-            for collumn in range(self.collumns):
-                if self.boops[collumn][row].booped == True:
-                    string += "1 "
-                else:
-                    string += "0 "
-            string += "\n"
-        return string
-"""
-"""
-    # return a string that looks like a collumn x row grid of boops (1 means booped, 0 means not booped).
-    def __str__(self):
-        string = ""
-        for collumn in self.boops:
-            for row in collumn:
-                if row.booped == True:
-                    string += "1 "
-                else:
-                    string += "0 "
-            string += "\n"
-        return string
-"""
-        
-
-
-"""
-    def __str__(self):
-        for collumn in self.boops:
-            for row in collumn:
-                print(row)
-
-        return "BoopBoard:\n"
-    
-    def __boop__(self, collumn, row):
-        self.boops[collumn][row].__boop__()
-"""
-
-"""
-def main():
-    boopboard = BoopBoard(4, 4)
-    print(boopboard)
-    boopboard.__boop__(0, 1)
-    print(boopboard)
-    boopboard.__boop__(2, 1)
-    print(boopboard)
-    boopboard.__boop__(3, 3)
-    print(boopboard)
-    boopboard.__boop__(3, 3)
-    print(boopboard)
-
-
-    print(boopboard.__isBooped__(1, 0))
-
-
-
-
-
-if __name__ == "__main__":
-    main()
-"""
--- booperapp1/views.py
+++ booperapp1/views.py
@@ -1,24 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-#import the boop and boop board classes from models.py on next line from 
-from . import models
-from .models import Boop, BoopBoard
-
+from .models import BoopBoard
 from django.views.generic import View
-import time
-import json
-from .models import Boop, BoopBoard
-
-
-# For the SSE view
-from django.views.decorators.csrf import csrf_exempt
 from django.core.serializers import serialize
+import json
 
 
 
 
-# Create your views here.
+
+
+"""
+This is the view for the Server-Sent Events (SSE) endpoint. It sends a stream of data to the client containing
+a json object of the Boop objects in the database. This is used to update the client's BoopBoard in close to real-time (~3 sec).
+"""
 class SSE(View):
+    # This function is called when the client sends a GET request to the SSE endpoint (about every 3 seconds).
     def get(self, request, *args, **kwargs):
         print("SSE view______GET")
         response = HttpResponse(content_type='text/event-stream')
@@ -26,8 +23,7 @@ class SSE(View):
         # response['Connection'] = 'keep-alive'
 
         # Retrieve data from the Boop model
-        justBoops = Boop.objects.all()
-
+        # justBoops = Boop.objects.all()
 
         # Retrieve data from the BoopBoard model
         boop_board = BoopBoard.objects.first()  # Assuming there's only one BoopBoard object
@@ -35,47 +31,43 @@ class SSE(View):
             print("BoopBoard exists")
             serversBoops = boop_board.boop_set.all()
 
-
-
-
         # Convert Boop objects to dictionary format
         boop_data = [{'id': boop.thisID, 'booped': boop.booped} for boop in serversBoops]
 
-
-
         for data in boop_data:
-            print("This is Data")
-            response.write(f"id: {data['id']}\n")
+            response.write(f"id: {data['id']}\n") # Send the Boop ID
             response.write(f"data: {json.dumps(data)}\n\n")  # Serialize data to JSON
-            # time.sleep(0.2)  # Simulate periodic updates
             response.flush()
             print("SSE response sent")
         
         return response
 
+
+# This is the view for the home page. It renders the buttons.html template (only called at the start of the client's session).
 def home(request):
     return render(request, "buttons.html")
 
 
+# This function sends the Server-Sent Events (SSE) to the client
 def send_sse(data):
     event = f"data: {data}\n\n"
     print("send_sse")
     return HttpResponse(event, content_type='text/event-stream')
 
+
+# This function is called when the client sends an ajax POST request to the update_button_state endpoint.
 def update_button_state_view(request):
-    print("update_button_state_view")
     if request.method == "POST":
-        print("update_button_state_view: request.method == POST")
+        print("update_button_state_view")
         button_id = request.POST.get("button_id")
-        print("Button id: " + button_id)
-        # print(serversBoops)
-        # button = serversBoops[int(button_id)-1]
-        boop_board = BoopBoard.objects.first()  # Assuming there's only one BoopBoard object
+        boop_board = BoopBoard.objects.first()  # The first (and only) BoopBoard object
         if boop_board:
-            print("BoopBoard exists")
             serversBoops = boop_board.boop_set.all()
             button = serversBoops.get(thisID=button_id)
             button.toggle_booped()
+        else:
+            print("BoopBoard does not exist")
+            return JsonResponse({'status': 'error', 'message': 'BoopBoard does not exist'})
 
         # Send SSE to all clients
         button_data = serialize('json', [button])
