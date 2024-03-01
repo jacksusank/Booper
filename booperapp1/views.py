3b0be7a4670022765f323b0c5685eced5ca5f840
JACK_SUSANK <jsusank5@gmail.com>
2024-03-01 02:20:10 -0800

    Optimized views

--- booperapp1/views.py
+++ booperapp1/views.py
@@ -8,10 +8,8 @@ import json
 import asyncio
 
 
-# Maintain a set of connected client IDs
+# Set that will contain a queue for each connected client
 connected_clients = set()
-masterQueue = asyncio.Queue()
-
 
 """
 This is the view for the Server-Sent Events (SSE) endpoint. It sends a stream of data to the client containing
@@ -21,32 +19,27 @@ class SSE(View):
     async def get(self, request, *args, **kwargs):
         # Asynchronous generator function to stream SSE events
         async def generate_sse_events():
-            masterQueue.put_nowait(1) # This is to ensure that whenever a client establishes an sse connection, they get the data
-            connected_clients.add(id(request))
+            myQueue = asyncio.Queue() # This is a queue that we will use to know when to send data to the client
+            myQueue.put_nowait(1) # This is to ensure that whenever a client establishes an sse connection, they get the data
+            connected_clients.add(myQueue) # Add the queue to the set of connected clients
             try:
-                while True:
-                    if not masterQueue.empty(): # If there is data in the queue then that means that there are changes that need to be sent to the clients
-                        # Retrieve data from the BoopBoard model
-                        masterQueue.get_nowait() # This returns and removes the first item in the queue
-                        boop_board = await sync_to_async(BoopBoard.objects.get)(name="Website BoopBoard") # The our serverside BoopBoard
-                        serversBoops = boop_board.boop_set.all() # This is a queryset of all the Boop objects in the BoopBoard
+                while await myQueue.get(): # This is an asynchronous operation that waits for the queue to have a value
+                    boop_board = await sync_to_async(BoopBoard.objects.get)(name="Website BoopBoard") # Our serverside BoopBoard
+                    serversBoops = boop_board.boop_set.all() # This is a queryset of all the Boop objects in the BoopBoard
 
-                        # Convert the queryset to an asynchronous operation
-                        serversBoops_async = await sync_to_async(list)(serversBoops)
+                    # Convert the queryset to an asynchronous operation
+                    serversBoops_async = await sync_to_async(list)(serversBoops)
+                    
+                    # Convert Boop objects to dictionary format
+                    boop_data = [{'id': boop.thisID, 'booped': boop.booped} for boop in serversBoops_async]
 
-                        
-                        # Convert Boop objects to dictionary format
-                        boop_data = [{'id': boop.thisID, 'booped': boop.booped} for boop in serversBoops_async]
-
-                        # Send SSE events for each Boop object to this client
-                        for data in boop_data:
-                            yield f"data: {json.dumps(data)}\n\n"
-                        
-                    else:
-                        await asyncio.sleep(0.1)
+                    # Send SSE events for each Boop object to this client
+                    for data in boop_data:
+                        yield f"data: {json.dumps(data)}\n\n" 
 
             except GeneratorExit: # If the client disconnects...
-                connected_clients.remove(id(request)) # ...then remove them from the set of connected clients...
+                connected_clients.remove(myQueue) # ...then remove them from the set of connected clients...
+                del myQueue # ...and delete the queue...
                 pass # ...and close the generator.
 
         # Return the SSE response with the asynchronous generator function
@@ -56,6 +49,10 @@ class SSE(View):
 # This is the view for the home page. It renders the buttons.html template (only called at the start of the client's session).
 def home(request):
     return render(request, "buttons.html")
+
+
+async def add_to_queue(queue, data):
+    await queue.put(data)
 
 
 # This function is called when any client sends an ajax POST request to the update_button_state endpoint.
@@ -68,11 +65,10 @@ def update_button_state_view(request):
             serversBoops = boop_board.boop_set.all()
             button = serversBoops.get(thisID=button_id)
             button.toggle_booped() # This changes the serverside state of the button
-            for i in range(connected_clients.__len__()):
-                masterQueue.put_nowait(button_id) 
-                # We add this arbitrary value to the queue this many times because we are going to send an sse to 
-                # each of the clients and for each one, we are going to take one item off of the queue
-
+            for client_queue in connected_clients: # For each client that is connected...
+                asyncio.run(add_to_queue(client_queue, button_id)) # ... add the button_id to their queue (this value is completely arbitrary, 
+                # but it will trigger the server to yield data to the client).
+                
         else:
             return JsonResponse({'status': 'error', 'message': 'BoopBoard does not exist'})
         return JsonResponse({'status': 'success', 'message': 'Button state updated successfully'})
